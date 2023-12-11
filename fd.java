84d1333d6a911925bd19b67d023aa7b7660b60a3
franklinfd <153469930+franklinfd@users.noreply.github.com>
2023-12-11 11:24:36 -0500

    Add files via upload

---
+++ fd.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author frdan8078
+ */
+public class fd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi, my name is Franklin Dang");
+    }
+    
+}
